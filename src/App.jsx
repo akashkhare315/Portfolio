6a24fdb79f9555415bd4dd0a89794379beb0577d
Akash khare <akashkhare135@gmail.com>
2025-09-20 18:14:17 +0530

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div>
       <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,52 +1,23 @@
 import React from "react";
-import logo from "../assets/logo.jpg"; // import image
+import { Link } from "react-router-dom";
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div className="w-full h-[20%] bg- flex items-center justify-between pr-50">
-      <img
-        href="#home"
-        src={logo}
-        alt="logo"
-        className="w-20 h-10 object-contain rounded-full"
-      />
-
-      <ul className="flex space-x-6 list-none">
-        <li>
-          <a
-            href="#home"
-            className="hover:underline hover:text-white font-normal"
-          >
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            href="#about"
-            className="hover:underline hover:text-white font-normal"
-          >
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            href="#projects"
-            className="hover:underline hover:text-white font-normal"
-          >
-            Projects
-          </a>
-        </li>
-        <li>
-          <a
-            href="#contact"
-            className="hover:underline hover:text-white font-normal"
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
-    </div>
+    <nav className="flex gap-6 p-4 bg-gray-900 text-white justify-center">
+      <Link to="/" className="hover:text-blue-400">
+        Home
+      </Link>
+      <Link to="/about" className="hover:text-blue-400">
+        About
+      </Link>
+      <Link to="/projects" className="hover:text-blue-400">
+        Projects
+      </Link>
+      <Link to="/contact" className="hover:text-blue-400">
+        Contact
+      </Link>
+    </nav>
   );
-}
+};
 
 export default Navbar;
